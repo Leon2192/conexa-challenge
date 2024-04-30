dca7fa64381d795c69f2ef30c63379e3d4c55018
leon_2192 <leon.strokes@gmail.com>
2024-04-30 17:16:17 -0300

    fix show rules + validation show characters

--- components/shared/EpissodesContainer/EpisodesContainer.tsx
+++ components/shared/EpissodesContainer/EpisodesContainer.tsx
@@ -68,44 +68,58 @@ const EpisodesContainer: React.FC = () => {
         <Loader />
       ) : (
         <>
-          <h1 className="text-center mt-4 text-xl   text-white">
-            Episode list
-          </h1>
+          {!selectedCharacter && !selectedCharacter2 && showPlayButton && (
+            <div>
+              <h1 className="text-center mt-6 text-4xl mb-2  text-white">
+                WELCOME!
+              </h1>
+              <p className="text-center mt-7 mx-2 text-white text-2xl">
+                Lets find the episodes of the television series Rick & Morty in
+                which the characters you select appear. You just need to choose
+                one character 1 and one character 2, and well take care of the
+                rest!
+              </p>
+            </div>
+          )}
           <div>
-            <div className="px-4 py-4 border-b border-gray-200">
-              <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
-                Episodes Character #1
-              </h2>
-              <EpisodeList
-                episodes={character1Episodes}
-                hasFilteredEpisodes={hasFilteredEpisodes}
-              />
-            </div>
+            {selectedCharacter && selectedCharacter2 && (
+              <div>
+                <div className="px-4 py-4 border-b border-gray-200">
+                  <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
+                    Episodes Character #1
+                  </h2>
+                  <EpisodeList
+                    episodes={character1Episodes}
+                    hasFilteredEpisodes={hasFilteredEpisodes}
+                  />
+                </div>
 
-            <div className="px-4 py-4 border-b border-gray-200">
-              <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
-                Shared Episodes Character #1 & Character #2
-              </h2>
-              <EpisodeList
-                episodes={sharedEpisodes}
-                hasFilteredEpisodes={hasFilteredEpisodes}
-              />
-            </div>
+                <div className="px-4 py-4 border-b border-gray-200">
+                  <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
+                    Shared Episodes Character #1 & Character #2
+                  </h2>
+                  <EpisodeList
+                    episodes={sharedEpisodes}
+                    hasFilteredEpisodes={hasFilteredEpisodes}
+                  />
+                </div>
 
-            <div className="px-4 py-4 border-b border-gray-200">
-              <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
-                Episodes Character #2
-              </h2>
-              <EpisodeList
-                episodes={character2Episodes}
-                hasFilteredEpisodes={hasFilteredEpisodes}
-              />
-            </div>
+                <div className="px-4 py-4 border-b border-gray-200">
+                  <h2 className="text-center mb-2 text-white text-2xl sm:text-3xl">
+                    Episodes Character #2
+                  </h2>
+                  <EpisodeList
+                    episodes={character2Episodes}
+                    hasFilteredEpisodes={hasFilteredEpisodes}
+                  />
+                </div>
+              </div>
+            )}
           </div>
           <div>
             {selectedCharacter || selectedCharacter2 ? (
               <p className="text-white text-center mt-4">
-                Buscaremos episodios en los que aparezcan
+                Buscaremos episodios en los que aparezcan{" "}
                 <span className="text-orange-600 font-bold text-xl underline">
                   {selectedCharacter?.name}
                 </span>
