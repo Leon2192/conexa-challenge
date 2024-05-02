9516cac98cb881bdbae461c9af3f809ae5627c2d
leon_2192 <leon.strokes@gmail.com>
2024-05-02 10:15:13 -0300

    fix imageDomains deprecated warning

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 
 const nextConfig = {
   images: {
-    domains: ["rickandmortyapi.com"],
+    remotePatterns: [
+      {
+        protocol: 'https', 
+        hostname: 'rickandmortyapi.com',
+      },
+    ],
   },
 };
 
